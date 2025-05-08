6ef3584fed22cb1c4ed3481bcb8f0cb31158e736
LSSandes <149780529+LSSandes@users.noreply.github.com>
2025-05-08 13:53:15 +0900

    style: update the button of whopcallbackview

--- src/views/WhopCallbackView.tsx
+++ src/views/WhopCallbackView.tsx
@@ -5,13 +5,13 @@ const WhopCallbackView: React.FC = () => {
   const code = params.get("code");
   return (
     <div className="flex justify-center items-center w-full min-h-screen">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-5">
         <img src={WHOP_LOGO} alt="" className="w-20 h-20 rounded-full" />
         <div className="flex justify-center items-center gap-2 text-gray-400">
           <span>Your code:</span>
           <span>{code}</span>
         </div>
-        <button className="flex justify-center items-center bg-[#FA4616] text-white rounded-md">
+        <button className="flex justify-center items-center p-2 bg-[#FA4616] text-white w-52 rounded-full">
           Get Access
         </button>
       </div>
